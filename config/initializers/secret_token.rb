b3a89d69dd163d879de5fd97294d5dbd9ddf07cd
Kyosuke INOUE <kyoro@hakamastyle.net>
2011-12-28 08:12:43 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imkyoro::Application.config.secret_token = 'a96f3c4f4387e68a4325d2d0a06b250f36c2f78da909d85113a31f5f27ed3454bcc6f5cd88c8f93b034d6c5e13e2d8f307250dda90015adaf990875288c44ae0'
